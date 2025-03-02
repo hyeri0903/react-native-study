a5e391427be6a662d08071c1a1acf9478c2b26d8
hyerijung <hyerijung0903@gmail.com>
2025-03-02 19:29:27 +0900

    fix EXPO_PUBLIC_APPWRITE_ENDPOINT

--- my-app/lib/appwrite.ts
+++ my-app/lib/appwrite.ts
@@ -3,7 +3,7 @@ import * as Linking from 'expo-linking';
 import { openAuthSessionAsync } from "expo-web-browser";
 
 export const config = {
-    platform: 'com.jhr.restate',
+    platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
 }
--- my-app/lib/global-provider.tsx
+++ my-app/lib/global-provider.tsx
@@ -4,18 +4,18 @@ import { getCurrentUser } from "./appwrite";
 import { useAppwrite } from "./useAppwrite";
 import { Redirect } from "expo-router";
 
-interface GlobalContextType {
-    isLoggedIn: boolean;
-    user: User | null;
-    loading: boolean;
-    refetch: () => void;
-}
-
 interface User {
     $id: string;
     name: string;
     email: string;
     avatar: string;
+}
+
+interface GlobalContextType {
+    isLoggedIn: boolean;
+    user: User | null;
+    loading: boolean;
+    refetch: (newParams?: Record<string, string | number>) => Promise<void>;
 }
 
 const GlobalContext = createContext<GlobalContextType | undefined>(undefined);
